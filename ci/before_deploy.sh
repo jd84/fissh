a576276b1dec293dff4309b337a55eb30990a5ca
jd84 <jd84@protonmail.com>
2019-03-26 13:18:28 +0100

    fixed before deploy

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -21,7 +21,7 @@ main() {
     # cross rustc --bin hello --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    # cp target/$TARGET/release/hello $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
